60221866401702163ee4d2d006919a741335392c
lambertn33 <lambertgithub@gmail.com>
2024-07-09 08:26:00 +0200

    feat: setup metrics

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 
+import { getMetrics, metricsMiddleware } from './metrics';
+
 import usersRoutes from './routes/users.route';
 
 import postsRoutes from './routes/posts.route';
 
 const app = express();
+
+app.use(metricsMiddleware);
+
+app.get('/metrics', getMetrics);
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
---
+++ apps/server/src/metrics.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import client from 'prom-client';
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+
+collectDefaultMetrics();
+
+export const httpRequestDurationMicroseconds = new client.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'status_code'],
+});
+
+export const metricsMiddleware = (req: any, res: any, next: any) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  res.on('finish', () => {
+    end({
+      method: req.method,
+      route: req.route ? req.route.path : '',
+      status_code: res.statusCode,
+    });
+  });
+  next();
+};
+
+export const getMetrics = async (req: any, res: any) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+};
